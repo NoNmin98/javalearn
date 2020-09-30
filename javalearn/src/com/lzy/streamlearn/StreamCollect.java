27f15ac8e0f0aedfc9f80559b15acabc950b21db
NoNmin98 <32301978+NoNmin98@users.noreply.github.com>
2020-09-30 08:42:44 +0800

    2020.9.30 fin.
    学完了！！！咱们下一个教程见，awt太老了弃掉

---
+++ javalearn/src/com/lzy/streamlearn/StreamCollect.java
@@ -0,0 +1,15 @@
+package com.lzy.streamlearn;
+
+import org.junit.Test;
+
+/**
+ * @author: lzy
+ * @description: Stream 终止操作 收集collect
+ * @date: 2020-09-28-10:03
+ */
+public class StreamCollect {
+    @Test
+    public void test1(){
+         
+    }
+}
